52383e21e2d5c8647ea239e3d5a2bb9a4bcb2f9a
Blerargh <49093336+Blerargh@users.noreply.github.com>
2026-01-22 21:41:22 +0800

    Add list tracking functionality in Spongebob

--- src/main/java/Spongebob.java
+++ src/main/java/Spongebob.java
@@ -1,26 +1,57 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Spongebob {
+    private ArrayList<String> textList = new ArrayList<>();
+
+    enum Actions {
+        LIST,
+        BYE,
+    }
+
+    public static void printTextList(ArrayList<String> textList) {
+        if (textList.isEmpty()) {
+            System.out.println("How about ordering a Krabby Patty first?");
+        } else {
+            for (int i = 0; i < textList.size(); i++) {
+                System.out.println("Krabby Patty Order " + (i + 1) + ": " + textList.get(i));
+            }
+        }
+    }
+
     public static void printHorizontalLine() {
         System.out.println("_________________________________________________________");
     }
+
     public static void main(String[] args) {
+        // Initialise Spongebob
+        Spongebob spongebob = new Spongebob();
+        
+        // Greeting message
         printHorizontalLine();
         System.out.println("Hello, I'm Spongebob Squarepants!\nWhat can I do for you today?");
         printHorizontalLine();
-        
-        // Read user input
+
+        // Parse user input
         Scanner scanner = new Scanner(System.in);
-        String userInput = scanner.nextLine();
-        
-        // Respond to user input
-        printHorizontalLine();
-        while (!userInput.equals("bye")) {
-            System.out.println(userInput);
+        while (true) {
+            String userInput = scanner.nextLine();
             printHorizontalLine();
-            userInput = scanner.nextLine();
+
+            String upperCaseUserInput = userInput.trim().toUpperCase();
+
+            // Handle order
+            if (upperCaseUserInput.equals(Actions.LIST.name())) {
+                printTextList(spongebob.textList);
+            } else if (upperCaseUserInput.equals(Actions.BYE.name())) {
+                break;
+            } else {
+                spongebob.textList.add(userInput);
+                System.out.println("Krabby Patty order received!");
+            }
             printHorizontalLine();
         }
+
         System.out.println("Goodbye! Have a great day under the sea!");
         printHorizontalLine();
         scanner.close();
