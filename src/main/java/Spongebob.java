f5a8953c5236ecd6504bccf28a6925d594fb571a
Blerargh <49093336+Blerargh@users.noreply.github.com>
2026-01-22 21:18:34 +0800

    Add echo feature for Spongebob

--- src/main/java/Spongebob.java
+++ src/main/java/Spongebob.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Spongebob {
     public static void printHorizontalLine() {
         System.out.println("_________________________________________________________");
@@ -6,7 +8,20 @@ public class Spongebob {
         printHorizontalLine();
         System.out.println("Hello, I'm Spongebob Squarepants!\nWhat can I do for you today?");
         printHorizontalLine();
-        System.out.println("Bye! Come back for another Krabby Patty soon!");
+        
+        // Read user input
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+        
+        // Respond to user input
+        printHorizontalLine();
+        while (!userInput.equals("bye")) {
+            System.out.println(userInput);
+            printHorizontalLine();
+            userInput = scanner.nextLine();
+            printHorizontalLine();
+        }
+        System.out.println("Goodbye! Have a great day under the sea!");
         printHorizontalLine();
     }
 }
