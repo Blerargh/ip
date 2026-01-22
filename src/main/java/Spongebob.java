6faf93ebe27b853cd7f42ef706135a2cc00f2c46
Blerargh <49093336+Blerargh@users.noreply.github.com>
2026-01-22 21:08:22 +0800

    Update Spongebob.java to greet and exit

---
+++ src/main/java/Spongebob.java
@@ -0,0 +1,12 @@
+public class Spongebob {
+    public static void printHorizontalLine() {
+        System.out.println("_________________________________________________________");
+    }
+    public static void main(String[] args) {
+        printHorizontalLine();
+        System.out.println("Hello, I'm Spongebob Squarepants!\nWhat can I do for you today?");
+        printHorizontalLine();
+        System.out.println("Bye! Come back for another Krabby Patty soon!");
+        printHorizontalLine();
+    }
+}
