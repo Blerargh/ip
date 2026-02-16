f3df523d9984f9cf3dac572ece877b4c9d41b209
Blerargh <49093336+Blerargh@users.noreply.github.com>
2026-02-16 17:09:31 +0800

    Set missing title for JavaFX Stage

--- src/main/java/spongebob/ui/GuiMain.java
+++ src/main/java/spongebob/ui/GuiMain.java
@@ -36,6 +36,7 @@ public class GuiMain extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Spongebob");
             fxmlLoader.<MainWindow>getController().setSpongebob(this.spongebob); // inject the Spongebob instance
             stage.show();
         } catch (IOException e) {
