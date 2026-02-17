120fd3575096f0d44f262af3e143c29c66b13719
Blerargh <49093336+Blerargh@users.noreply.github.com>
2026-02-17 19:34:20 +0800

    Fix checkstyle violations

--- src/test/java/spongebob/commands/subcommands/AddReservationCommandTest.java
+++ src/test/java/spongebob/commands/subcommands/AddReservationCommandTest.java
@@ -17,25 +17,21 @@ public class AddReservationCommandTest {
     public void assertValidInput_missingSeparators_exceptionThrown() {
         AddReservationCommand addReservationCommand1 = new AddReservationCommand(
                 "Patrick's Birthday /from 2024-12-31 19:00");
-        Assertions.assertThrows(SpongebobException.class,
-                () -> addReservationCommand1.assertValidInput());
+        Assertions.assertThrows(SpongebobException.class, () -> addReservationCommand1.assertValidInput());
 
         AddReservationCommand addReservationCommand2 = new AddReservationCommand(
                 "Patrick's Birthday /to 2024-12-31 21:00");
-        Assertions.assertThrows(SpongebobException.class,
-                () -> addReservationCommand2.assertValidInput());
+        Assertions.assertThrows(SpongebobException.class, () -> addReservationCommand2.assertValidInput());
 
         AddReservationCommand addReservationCommand3 = new AddReservationCommand(
                 "Patrick's Birthday 2024-12-31 21:00");
-        Assertions.assertThrows(SpongebobException.class,
-                () -> addReservationCommand3.assertValidInput());
+        Assertions.assertThrows(SpongebobException.class, () -> addReservationCommand3.assertValidInput());
     }
 
     @Test
     public void assertValidInput_multipleSeparators_exceptionThrown() {
         AddReservationCommand addReservationCommand1 = new AddReservationCommand(
                 "Patrick's Birthday /from 2024-12-31 19:00 /to 2024-12-31 21:00 /to 2024-12-31 22:00");
-        Assertions.assertThrows(SpongebobException.class,
-                () -> addReservationCommand1.assertValidInput());
+        Assertions.assertThrows(SpongebobException.class, () -> addReservationCommand1.assertValidInput());
     }
 }
